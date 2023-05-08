2ac4ccb1f7ee4815260a66cf4c05aa411d5a8577
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-05-08 19:33:21 +0300

    getFirms-axios added

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,17 +1,40 @@
-import { Button, Typography } from '@mui/material'
-import React from 'react'
+import { Button, Typography } from "@mui/material";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 
 const Firms = () => {
-  return (
-    <div>
-      <Typography variant='h4' color="error" mb={3}>
-        Firm
-      </Typography>
-      <Button variant='contained'>
-        New Firm 
-      </Button>
-    </div>
-  )
-}
+  const { token } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-export default Firms
+  const getFirms = async () => {
+    const BASE_URL = "https://12312.fullstack.clarusway.com/";
+
+
+    const url = "firms"
+    try {
+      dispatch(fetchStart());
+      const { data } = await axios(`${BASE_URL}stock/firms/`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+      dispatch(getSuccess({data, url}));
+    } catch (error) {
+      console.log(error);
+      dispatch(fetchFail())
+    }
+
+    useEffect(() => {
+      getfirms();
+    }, []);
+
+    return (
+      <div>
+        <Typography variant="h4" color="error" mb={3}>
+          Firm
+        </Typography>
+        <Button variant="contained">New Firm</Button>
+      </div>
+    );
+  };
+};
+export default Firms;
