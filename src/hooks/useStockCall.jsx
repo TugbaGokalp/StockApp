7d4b46b54f71b0012bd5802d5bed29c29f45af23
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-05-10 12:16:27 +0300

    useAxios is used

--- src/hooks/useStockCall.jsx
+++ src/hooks/useStockCall.jsx
@@ -1,19 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
 import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 import axios from "axios";
+import useAxios from "./useAxios";
 
 const useStockCall = () => {
   const dispatch = useDispatch();
   const { token } = useSelector((state) => state.auth);
+  const { axiosWithToken } = useAxios(); // take axiosWithToken from useAxios
 
   const getStockData = async (url) => {
-    const BASE_URL = "https://12312.fullstack.clarusway.com/";
-
+    // const BASE_URL = "https://12312.fullstack.clarusway.com/";
+    dispatch(fetchStart());
     try {
-      dispatch(fetchStart());
-      const { data } = await axios(`${BASE_URL}stock/${url}/`, {
-        headers: { Authorization: `Token ${token}` },
-      });
+      // const { data } = await axios(`${BASE_URL}stock/${url}/`, {
+      //   headers: { Authorization: `Token ${token}` },
+      // });
+
+      const { data } = await axiosWithToken(`stock/${url}/`);
+      // get the data from axiosWithToken. don't have to write .get because it's process of get
+      // base url + stock/${url}/
       dispatch(getSuccess({ data, url }));
     } catch (error) {
       console.log(error);
@@ -21,24 +26,19 @@ const useStockCall = () => {
     }
   };
 
-
   const deleteStockData = async (url, id) => {
     const BASE_URL = "https://12312.fullstack.clarusway.com/";
-
+    dispatch(fetchStart());
     try {
-      dispatch(fetchStart());
-      await axios.delete(`${BASE_URL}stock/${url}/${id}/ `, {
-        headers: { Authorization: `Token ${token}` },
+      await axiosWithToken.delete(`stock/${url}/${id}/ `, {
+        
       });
-      getStockData({url})
+      getStockData({ url });
     } catch (error) {
       console.log(error);
       dispatch(fetchFail());
     }
   };
-
-
-
 
   return { getStockData };
 };
