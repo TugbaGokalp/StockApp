ec46b5cea157f73672b6d56f1d072ae15517d746
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-05-10 18:05:07 +0300

    globalStyle was created

---
+++ src/styles/globalStyle.jsx
@@ -0,0 +1,11 @@
+export const flex = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 1,
+}
+
+export const btnStyle = {
+    cursor: "pointer",
+    "&:hover":{color: "red"},
+}
