44587787bbe7cab3d2a0aee6f81c8ab81eead2f6
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-05-12 17:06:31 +0300

    modal style was added

--- src/styles/globalStyle.jsx
+++ src/styles/globalStyle.jsx
@@ -1,11 +1,23 @@
 export const flex = {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    gap: 2,
-}
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: 2,
+};
 
 export const btnStyle = {
-    cursor: "pointer",
-    "&:hover":{color: "red"},
-}
+  cursor: "pointer",
+  "&:hover": { color: "red" },
+};
+
+export const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #00000058",
+  boxShadow: 24,
+  p: 4,
+};
