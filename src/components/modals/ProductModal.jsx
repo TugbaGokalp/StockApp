883be062a7b71d78e45e1bbbb665c42e9d0802eb
tugba-dotcom <tugbagokalp7265@gmail.com>
2023-05-14 20:32:58 +0300

    product page finished

--- src/components/modals/ProductModal.jsx
+++ src/components/modals/ProductModal.jsx
@@ -1,45 +1,43 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import { modalStyle } from "../../styles/globalStyle";
-import TextField from "@mui/material/TextField";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import { Button } from "@mui/material";
-import useStockCall from "../../hooks/useStockCall";
-import { useSelector } from "react-redux";
+import Box from "@mui/material/Box"
+import Modal from "@mui/material/Modal"
+import InputLabel from "@mui/material/InputLabel"
+import MenuItem from "@mui/material/MenuItem"
+import FormControl from "@mui/material/FormControl"
+import Select from "@mui/material/Select"
+
+import { modalStyle } from "../../styles/globalStyle"
+import TextField from "@mui/material/TextField"
+import { Button } from "@mui/material"
+import useStockCall from "../../hooks/useStockCall"
+import { useSelector } from "react-redux"
 
 export default function ProductModal({ open, handleClose, info, setInfo }) {
-  const { postStockData } = useStockCall();
-  const { categories } = useSelector((state) => state.stock);
+  const { postStockData } = useStockCall()
+  const { categories, brands } = useSelector((state) => state.stock)
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setInfo({ ...info, [name]: value });
-  };
+    const { name, value } = e.target
+    setInfo({ ...info, [name]: value })
+  }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-
-    postStockData("firms", info);
-
-    handleClose();
+    e.preventDefault()
+    postStockData("products", info)
+    handleClose()
     setInfo({
-      category: "",
-      brand: "",
+      category_id: "",
+      brand_id: "",
       name: "",
-    });
-  };
+    })
+  }
+
   return (
     <div>
       <Modal
         open={open}
         onClose={() => {
-          handleClose();
-          setInfo({ category: "", brand: "", name: "" });
-          //also used to reset information in modal when clicked outside the modal.
+          handleClose()
+          setInfo({ category_id: "", brand_id: "", name: "" })
         }}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
@@ -51,70 +49,63 @@ export default function ProductModal({ open, handleClose, info, setInfo }) {
             onSubmit={handleSubmit}
           >
             <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Category</InputLabel>
+              <InputLabel id="demo-simple-select-label">Categories</InputLabel>
               <Select
                 labelId="category"
                 id="category"
-                // value={age}
+                name="category_id"
+                value={info?.category_id}
                 label="Category"
                 onChange={handleChange}
               >
                 {categories?.map((item) => (
-                  <MenuItem key={item.id} value={10}>
+                  <MenuItem key={item.id} value={item.id}>
                     {item.name}
                   </MenuItem>
                 ))}
               </Select>
             </FormControl>
+            <FormControl fullWidth>
+              <InputLabel variant="outlined" id="brand-select">
+                Brands
+              </InputLabel>
+              <Select
+                labelId="brand-select"
+                label="Brand"
+                id="brand-select"
+                name="brand_id"
+                value={info?.brand_id}
+                onChange={handleChange}
+                required
+              >
+                {brands?.map((brand) => {
+                  return (
+                    <MenuItem key={brand.id} value={brand.id}>
+                      {brand.name}
+                    </MenuItem>
+                  )
+                })}
+              </Select>
+            </FormControl>
+
             <TextField
-              label="Firm Name"
+              margin="dense"
+              label="Product Name"
               name="name"
               id="name"
-              type="name"
+              type="text"
               variant="outlined"
               value={info?.name}
               onChange={handleChange}
               required
             />
-            <TextField
-              label="Phone"
-              name="phone"
-              id="phone"
-              type="tel"
-              variant="outlined"
-              value={info?.phone}
-              onChange={handleChange}
-              required
-            />
-
-            <TextField
-              label="Address"
-              name="address"
-              id="address"
-              type="text"
-              variant="outlined"
-              required
-              value={info?.address}
-              onChange={handleChange}
-            />
-
-            <TextField
-              label="Image"
-              name="image"
-              id="image"
-              type="url"
-              variant="outlined"
-              required
-              value={info?.image}
-              onChange={handleChange}
-            />
 
             <Button type="submit" variant="contained">
-              Submit Form
+              Add New Product
             </Button>
           </Box>
         </Box>
       </Modal>
     </div>
-  );
+  )
 }
